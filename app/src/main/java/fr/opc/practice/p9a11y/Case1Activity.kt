7ef32d868a8cd0885d1ec593fe37d74553bb801e
Samuel Lebailly <sam.meusa@gmail.com>
2024-07-15 16:15:24 +0200

    feat(main/case1): Refactoring of the activity + add documentations.

--- app/src/main/java/fr/opc/practice/p9a11y/Case1Activity.kt
+++ app/src/main/java/fr/opc/practice/p9a11y/Case1Activity.kt
@@ -5,30 +5,59 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import fr.opc.practice.p9a11y.databinding.ActivityCase1Binding
 
+/**
+ * The activity for the first case - Being able to add a certain quantity of a product to my basket.
+ */
 class Case1Activity : AppCompatActivity() {
     private lateinit var binding: ActivityCase1Binding
+    private var quantity = 0
 
+    /**
+     * Life cycle method - Called when the activity is created.
+     * @param savedInstanceState The saved instance state.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCase1Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        var quantity = 0
+        updateUIQuantity(quantity)
+        initListeners()
+    }
 
-        binding.quantityText.text = "$quantity"
-        binding.addButton.setOnClickListener {
-            quantity++
-            binding.quantityText.text = "$quantity"
-        }
+    /**
+     * Method to initialize the listeners on the buttons.
+     */
+    private fun initListeners() {
+        binding.addButton.setOnClickListener { incrementQuantity() }
+        binding.removeButton.setOnClickListener { decrementQuantity() }
+    }
 
-        binding.removeButton.setOnClickListener {
-            if (quantity > 0) {
-                quantity--
-                binding.quantityText.text = "$quantity"
-            } else {
-                Toast.makeText(this, getString(R.string.impossible_d_avoir_une_quantit_n_gative), Toast.LENGTH_SHORT)
-                    .show()
-            }
+    /**
+     * Method to increment the quantity of the product in the basket.
+     */
+    private fun incrementQuantity() {
+        quantity++
+        updateUIQuantity(quantity)
+    }
+
+    /**
+     * Method to decrement the quantity of the product in the basket.
+     */
+    private fun decrementQuantity() {
+        if (quantity > 0) {
+            quantity--
+            updateUIQuantity(quantity)
+        } else {
+            Toast.makeText(this, getString(R.string.impossible_d_avoir_une_quantit_n_gative), Toast.LENGTH_SHORT).show()
         }
     }
+
+    /**
+     * Method to update the UI with the quantity of the product in the basket.
+     * @param quantity The quantity of the product in the basket.
+     */
+    private fun updateUIQuantity(quantity: Int){ binding.quantityText.text = "$quantity"}
+
+
 }
